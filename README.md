acd0edaf3349966863137fcee47a6be206ad76ee
THanyaTHornnn <woonnasena49@gmail.com>
2025-02-11 01:11:15 +0700

    complete

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:Thanyathorn Pratuman
+# STUDENT ID:670612124
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1, Rect *R2){
+    double w = max(0.0, min(R1->x + R1->w, R2->x + R2->w) - max(R1->x, R2->x));
+    double h = max(0.0, min(R1->y, R2->y) - max(R1->y - R1->h, R2->y - R2->h));
+    
+    return w * h;
 }
